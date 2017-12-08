1f3f4398625dca7fda3d00f214540426bc6edfcf
Sarmiento <sarmife@prd.manulifeusa.com>
2017-12-08 10:24:15 -0500

    up

--- src/main/java/org/mahikero01/bookstore/repository/BookRepository.java
+++ src/main/java/org/mahikero01/bookstore/repository/BookRepository.java
@@ -5,9 +5,14 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
+import javax.transaction.Transactional;
 
 import org.mahikero01.bookstore.model.Book;
 
+import static javax.transaction.Transactional.TxType.REQUIRED;
+import static javax.transaction.Transactional.TxType.SUPPORTS;
+
+@Transactional(SUPPORTS)
 public class BookRepository {
 	
 	@PersistenceContext(unitName="bookStorePU")
@@ -17,11 +22,13 @@ public class BookRepository {
 		return em.find(Book.class, id);
 	}
 	
+	@Transactional(REQUIRED)
 	public Book create(Book book) {
 		em.persist(book);
 		return book;
 	}
 	
+	@Transactional(REQUIRED)
 	public void delete(Long id) {
 		em.remove(em.getReference(Book.class, id));
 	}
