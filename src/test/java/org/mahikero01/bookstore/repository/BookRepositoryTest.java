de149606a17a63f32ee5cf83af9ef7f6c5eb50a5
Federico Paras Sarmiento <federicoparassarmiento@yahoo.com>
2017-12-10 06:31:59 +0800

    up

---
+++ src/test/java/org/mahikero01/bookstore/repository/BookRepositoryTest.java
@@ -0,0 +1,14 @@
+package org.mahikero01.bookstore.repository;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BookRepositoryTest {
+
+	@Test
+	public void testCreate() {
+		fail("Not yet implemented");
+	}
+
+}
